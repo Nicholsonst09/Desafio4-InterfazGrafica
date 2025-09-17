136c94926d7fbc2c63e063be1c90f222b67c2872
NichoStrick <nicoparedes00@gmail.com>
2025-09-16 23:07:09 -0300

    Implementar interface 'HabitatOceano'

---
+++ ecosistema-oceano/src/desafio4/HabitatOceano.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package desafio4;
+
+
+public interface HabitatOceano {
+    void nadar();
+    void bucear (int profundidad);
+    void navegarCorrientes();
+}
