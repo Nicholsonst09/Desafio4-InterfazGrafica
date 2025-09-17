e3f917ee9660d3b40580510a81167c6e994a7aba
Luciano Miranda <lu.mer.zoni@gmail.com>
2025-09-17 00:03:38 -0300

    creacion de metodos heredados de la clase Animal y habitatOceano (Tiburón)

--- ecosistema-oceano/src/desafio4/TiburonBlanco.java
+++ ecosistema-oceano/src/desafio4/TiburonBlanco.java
@@ -17,9 +17,9 @@ class TiburonBlanco extends Animal implements HabitatOceano {
         super(nombre, "Tiburón Blanco", peso, altura);
         this.velocidadMaxima = 60;
     }
-    
-      @Override
-     public void hacerSonido() {
+
+    @Override
+    public void hacerSonido() {
         if (!vivo || energia < 3) {
             System.out.println("El tiburón " + nombre + " no puede hacer sonido.");
             return;
@@ -27,5 +27,38 @@ class TiburonBlanco extends Animal implements HabitatOceano {
         System.out.println("El tiburón " + nombre + " se acerca silenciosamente con un leve sonido.");
         consumirEnergia(3);
     }
+
+    @Override
+    public void moverse() {
+        if (!vivo || energia < 12) {
+            System.out.println("El tiburón " + nombre + " no puede moverse.");
+            return;
+        }
+        System.out.println("El tiburón " + nombre + " se mueve con velocidad buscando su presa.");
+        consumirEnergia(12);
+    }
+    
+        @Override
+    public void alimentarse() {
+        if (!vivo || energia < 20) {
+            System.out.println("El tiburón " + nombre + " no puede alimentarse.");
+            return;
+        }
+        System.out.println("El tiburón " + nombre + " devora su presa!");
+        energia = Math.min(100, energia + 35);
+        consumirEnergia(20);
+    }
+    
+    @Override
+    public void nadar() {
+        if (!vivo || energia < 10) {
+            System.out.println("El tiburón " + nombre + " no puede nadar.");
+            return;
+        }
+        System.out.println("El tiburón " + nombre + " nada con movimientos perfectos.");
+        consumirEnergia(10);
+    }
+    
+    
 
 }
